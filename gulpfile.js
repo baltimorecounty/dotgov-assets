83cd80efaaf1c74aa4f21c6faaa38007455f2c83
Marty Powell <jpowel25@gmail.com>
2018-10-03 10:32:10 -0400

    added gulp to the project

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+const basePath = './src';
+const baseSassPath = `${basePath}/stylesheets`;
+const distPath = './dist';
+const stylesheets = [
+	`${baseSassPath}/bc-main.scss`,
+	`${baseSassPath}/dotgov-main.scss`
+];
+
+const compileSass = (path) => {
+	return gulp.src(path)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(`${distPath}/stylesheets`));
+};
+
+gulp.task('sass', () => {
+	stylesheets.forEach((stylesheet) => {
+		compileSass(stylesheet);
+	})
+});
+
+gulp.task('build', ['sass']);
