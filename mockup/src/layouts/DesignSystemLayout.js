2ec3c7c82633abfd310e3c6b7b4af75399684ed5
martypowell <jpowel25@gmail.com>
2018-10-04 09:41:38 -0400

    removed unused code

--- mockup/src/layouts/DesignSystemLayout.js
+++ mockup/src/layouts/DesignSystemLayout.js
@@ -1,24 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-const propTypes = {};
-
-const defaultProps = {};
 
 export default class DesignSystemLayout extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-
-	render() {
-		return (
-			<React.Fragment>
-				{this.props.children}
-			</React.Fragment>
-		);
-	}
+    render() {
+        return <React.Fragment>{this.props.children}</React.Fragment>;
+    }
 }
-
- DesignSystemLayout.propTypes = propTypes;
- DesignSystemLayout.defaultProps = defaultProps;
--- mockup/src/layouts/Layout.js
+++ mockup/src/layouts/Layout.js
@@ -1,16 +1,7 @@
 import React from 'react';
 
 export default class Layout extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-
-	render() {
-		return (
-			<React.Fragment>
-				{this.props.children}
-			</React.Fragment>
-		);
-	}
+    render() {
+        return <React.Fragment>{this.props.children}</React.Fragment>;
+    }
 }
