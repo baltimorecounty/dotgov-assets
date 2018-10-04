a12c3e2db0bd854bbf25c3a1f1502759d90eaffc
martypowell <jpowel25@gmail.com>
2018-10-04 09:29:11 -0400

    intial revision

---
+++ mockup/src/pages/DesignSystem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '../components/Typography/Index';
+
+export default class DesignSystem extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<Typography />
+			</React.Fragment>
+		);
+	}
+}
