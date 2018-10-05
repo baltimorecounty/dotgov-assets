c358f51ac0f7a31e162c62593b800506dae573d0
martypowell <jpowel25@gmail.com>
2018-10-05 13:56:56 -0400

    updates

--- mockup/src/layouts/DesignSystemLayout.js
+++ mockup/src/layouts/DesignSystemLayout.js
@@ -1,7 +1,119 @@
 import React from 'react';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 export default class DesignSystemLayout extends React.Component {
+    state = {
+		collapsed: false,
+		breadCrumbs: []
+	};
+
+	componentWillReceiveProps(nextProps) {
+		this.setBreadCrumbsFromPath(nextProps);
+	}
+
+	setBreadCrumbsFromPath(nextProps) {
+		const { pathname } = nextProps.location;
+		const breadCrumbs = pathname.split('/');
+		this.setState({
+			breadCrumbs
+		});
+	}
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    };
+
     render() {
-        return <React.Fragment>{this.props.children}</React.Fragment>;
+        return (
+            <Layout>
+                <Header className="header">
+                    <div className="logo" />
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        style={{ lineHeight: '64px' }}
+                    >
+                        <Menu.Item key="1">nav 1</Menu.Item>
+                        <Menu.Item key="2">nav 2</Menu.Item>
+                        <Menu.Item key="3">nav 3</Menu.Item>
+                    </Menu>
+                </Header>
+                <Layout>
+                    <Sider width={200} style={{ background: '#fff' }}>
+                        <Menu
+                            mode="inline"
+                            defaultSelectedKeys={['1']}
+                            defaultOpenKeys={['sub1']}
+                            style={{ height: '100%', borderRight: 0 }}
+                        >
+                            <SubMenu
+                                key="sub1"
+                                title={
+                                    <span>
+                                        <Icon type="user" />
+                                        subnav 1
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="1">option1</Menu.Item>
+                                <Menu.Item key="2">option2</Menu.Item>
+                                <Menu.Item key="3">option3</Menu.Item>
+                                <Menu.Item key="4">option4</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub2"
+                                title={
+                                    <span>
+                                        <Icon type="laptop" />
+                                        subnav 2
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="5">option5</Menu.Item>
+                                <Menu.Item key="6">option6</Menu.Item>
+                                <Menu.Item key="7">option7</Menu.Item>
+                                <Menu.Item key="8">option8</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub3"
+                                title={
+                                    <span>
+                                        <Icon type="notification" />
+                                        subnav 3
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="9">option9</Menu.Item>
+                                <Menu.Item key="10">option10</Menu.Item>
+                                <Menu.Item key="11">option11</Menu.Item>
+                                <Menu.Item key="12">option12</Menu.Item>
+                            </SubMenu>
+                        </Menu>
+                    </Sider>
+                    <Layout style={{ padding: '0 24px 24px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+							{this.state.breadCrumbs.length && this.state.breadCrumbs.map((breadCrumb, breadCrumbIndex) => {
+								return <Breadcrumb.Item key={breadCrumbIndex}>{breadCrumb}</Breadcrumb.Item>;
+							})}
+                        </Breadcrumb>
+                        <Content
+                            style={{
+                                padding: 24,
+                                margin: 0,
+                                minHeight: 280
+                            }}
+                        >
+                            {this.props.children}
+                        </Content>
+                    </Layout>
+                </Layout>
+            </Layout>
+        );
     }
 }
