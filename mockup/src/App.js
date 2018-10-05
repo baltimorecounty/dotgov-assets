509074f1b601f5966908b0aca24e19582d718c49
martypowell <jpowel25@gmail.com>
2018-10-05 13:56:13 -0400

    moved router logic into this file

--- mockup/src/App.js
+++ mockup/src/App.js
@@ -1,20 +1,58 @@
-import React, {Component} from 'react';
-import {Route} from 'react-router';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
+import { HashRouter } from 'react-router-dom';
 import HomePage from './pages/Homepage';
 import DesignSystem from './pages/DesignSystem';
-import Layout from './layouts/Layout';
+import Typography from './components/Typography/Index.js';
+import DefaultLayout from './layouts/Layout';
+import DesignSystemLayout from './layouts/DesignSystemLayout';
+import 'antd/dist/antd.css';
 import './App.css';
+import './bc-main.css';
+import './dotgov-main.css';
+
+const RouteWithLayout = ({ layout, component, ...rest }) => {
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                React.createElement(
+                    layout,
+                    props,
+                    React.createElement(component, props)
+                )
+            }
+        />
+    );
+};
 
 class App extends Component {
-  render() {
-    const rootPath = process.env.PUBLIC_URL;
-    return (
-      <Layout>
-        <Route exact path={`${rootPath}/`} component={HomePage}/>
-        <Route exact path={`${rootPath}/design-system`} component={DesignSystem}/>
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <HashRouter basename="/">
+                <Switch>
+                    <RouteWithLayout
+                        exact
+                        path={`/`}
+                        layout={DefaultLayout}
+                        component={HomePage}
+                    />
+                    <RouteWithLayout
+                        exact
+                        layout={DesignSystemLayout}
+                        path={`/design-system`}
+                        component={DesignSystem}
+                    />
+					<RouteWithLayout
+                        exact
+                        layout={DesignSystemLayout}
+                        path={`/design-system/typography`}
+                        component={Typography}
+                    />
+                </Switch>
+            </HashRouter>
+        );
+    }
 }
 
 export default App;
