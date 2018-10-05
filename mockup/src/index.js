162b2ec58bb800acfeff6e9527144531b132eb72
martypowell <jpowel25@gmail.com>
2018-10-05 13:56:27 -0400

    removed router logic

--- mockup/src/index.js
+++ mockup/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <HashRouter basename="/">
-        <App />
-    </HashRouter>,
+
+        <App />,
     document.getElementById('root')
 );
 
