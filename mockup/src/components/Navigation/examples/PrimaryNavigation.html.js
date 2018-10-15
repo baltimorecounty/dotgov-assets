480b4784ea9dd70e927bf26530074563e6c6a2ab
martypowell <jpowel25@gmail.com>
2018-10-15 14:57:13 -0400

    added sample code

---
+++ mockup/src/components/Navigation/examples/PrimaryNavigation.html.js
@@ -0,0 +1,7 @@
+export default `<ul class="bc_nav-list">
+   <li><a href="/departments-and-services"><i class="fas fa-list-ul"></i> Departments and Services</a></li>
+   <li><a href="/pay"><i class="fas fa-shopping-cart"></i> Pay</a></li>
+   <li><a href="/apply"><i class="fas fa-edit"></i> Apply</a></li>
+   <li><a href="/report"><i class="fas fa-exclamation-triangle"></i> Report a Problem</a></li>
+   <li><a href="/contact"><i class="fas fa-envelope"></i> Contact Us</a></li>
+</ul>`;
