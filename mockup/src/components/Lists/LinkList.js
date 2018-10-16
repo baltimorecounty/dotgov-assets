c9046de3c5b073fcd5294f9c29ca305f6d9f34bb
Marty Powell <jepowell@baltimorecountymd.gov>
2018-10-16 07:57:21 -0400

    added component

---
+++ mockup/src/components/Lists/LinkList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    items: PropTypes.array.isRequired
+};
+
+export default class LinkList extends React.Component {
+    render() {
+        return (
+            <ul className="bc_link-list">
+                {this.props.items.map((item, itemKey) => (
+                    <li key={itemKey}>
+                        <a href={item.link}>
+                            <i className={`fas fa-${item.icon}`} /> {item.name}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+LinkList.propTypes = propTypes;
+LinkList.defaultProps = defaultProps;
