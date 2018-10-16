bafa70d314fb0fd30887b6d1062b644fc7f8fa8f
Marty Powell <jepowell@baltimorecountymd.gov>
2018-10-16 07:58:17 -0400

    added baseline for docs

---
+++ mockup/src/components/Navigation/Secondary/Index.js
@@ -0,0 +1,25 @@
+ import React from 'react';
+ import PropTypes from 'prop-types';
+ import Section from '../../DesignSystem/Section';
+
+ const propTypes = {};
+
+ const defaultProps = {};
+
+ export default class Index extends React.Component {
+	 constructor(props) {
+		 super(props);
+		 this.state = {};
+	 }
+
+	 render() {
+		 return (
+			 <React.Fragment>
+
+			 </React.Fragment>
+		 );
+	 }
+ }
+
+  Index.propTypes = propTypes;
+  Index.defaultProps = defaultProps;
