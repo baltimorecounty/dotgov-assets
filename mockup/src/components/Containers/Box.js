47f8d88e57f595c7991363a348c253391d1a338f
martypowell <jpowel25@gmail.com>
2018-10-16 13:13:37 -0400

    ensure css classe are applied

--- mockup/src/components/Containers/Box.js
+++ mockup/src/components/Containers/Box.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default class Box extends React.Component {
 	render() {
+		const { className } = this.props;
+		const cssClasses = `bc_box ${className ? ` ${className}` : ``}`;
 		return (
-			<div className="bc_box">
+			<div className={cssClasses}>
 				{this.props.children}
 			</div>
 		);
