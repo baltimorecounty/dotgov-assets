8ea68ef5f8ac5aa3259b0b577e1414ce89e2dbd6
Marty Powell <jepowell@baltimorecountymd.gov>
2018-10-16 08:02:55 -0400

    initial revision

---
+++ mockup/src/components/Navigation/SecondaryNavigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LinkList from '../Lists/LinkList';
+import secondaryNavItems from '../../data/secondary-navigation.json';
+
+export default class SecondaryNavigation extends React.Component {
+	render() {
+		return (
+			<nav className="dg_secondary-nav">
+				<LinkList items={secondaryNavItems} />
+			</nav>
+		);
+	}
+}
