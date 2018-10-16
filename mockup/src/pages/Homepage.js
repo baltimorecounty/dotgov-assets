ad3fc4bff235fb9a3a85c7fb1577e154d2d35542
Marty Powell <jepowell@baltimorecountymd.gov>
2018-10-16 09:36:28 -0400

    refactored out code into seperate component

--- mockup/src/pages/Homepage.js
+++ mockup/src/pages/Homepage.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PrimaryNavigation from '../components/Navigation/PrimaryNavigation';
-import SecondaryNavigation from '../components/Navigation/SecondaryNavigation';
+import SiteHeader from '../components/Site/Header';
 
 export default class Homepage extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-
 	render() {
 		return (
 			<React.Fragment>
-				<header>
-					<SecondaryNavigation />
-					<PrimaryNavigation />
-				</header>
+				<SiteHeader />
 				<h1>Baltimore County Government</h1>
 				<p>this is a placeholder page for the homepage</p>
 				<Link to='/design-system'>Design System</Link>
