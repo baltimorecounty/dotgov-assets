abba408cc193ec65e7fd65a02367e817be683f22
martypowell <jpowel25@gmail.com>
2018-11-05 15:03:13 -0500

    added component for transparency

--- mockup/src/components/Homepage/TransparencyInGovernment.js
+++ mockup/src/components/Homepage/TransparencyInGovernment.js
@@ -3,10 +3,10 @@ import CalloutList from '../Callouts/CalloutList/CalloutList';
 import CalloutData from '../../data/transparency-in-government.json';
 
 export default function TransparencyInGovernment(props) {
-	return (
-		<React.Fragment>
-			<h2 className="alt-heading">Transparency In Government</h2>
-			<CalloutList items={CalloutData} />
-		</React.Fragment>
-	);
+    return (
+        <div className="bc_section">
+            <h2 className="alt-heading">Transparency In Government</h2>
+            <CalloutList items={CalloutData} />
+        </div>
+    );
 }
