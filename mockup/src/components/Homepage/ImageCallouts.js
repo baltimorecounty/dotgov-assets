0d7a62e1a6189b1c403712a5d67aa8cf0c4f6074
martypowell <jpowel25@gmail.com>
2018-11-07 11:41:43 -0500

    converted to use flexboxgrid

--- mockup/src/components/Homepage/ImageCallouts.js
+++ mockup/src/components/Homepage/ImageCallouts.js
@@ -4,9 +4,9 @@ import ImageCallout from '../Callouts/ImageCallout/ImageCallout.js';
 
 export default function MostPopularServices(props) {
     return (
-        <div className="bc_grid bc_grid-cols-2 bc_section dg_homepage-callouts">
+        <div className="row between-xs bc_section dg_homepage-callouts">
             {callOuts.map(callout => (
-                <ImageCallout className="dg_callout bc_col" {...callout} />
+                <ImageCallout className="dg_callout" {...callout} />
             ))}
         </div>
     );
