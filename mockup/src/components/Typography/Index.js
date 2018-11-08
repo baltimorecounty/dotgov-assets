5742473674ee62efca8dffc646ddd003b77ddd66
martypowell <jpowel25@gmail.com>
2018-11-08 07:50:09 -0500

    reformat

--- mockup/src/components/Typography/Index.js
+++ mockup/src/components/Typography/Index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Headings from './Headings';
 
 export default class Index extends React.Component {
-	state = {
-		componentFriendlyName: "Typography"
-	}
+    state = {
+        componentFriendlyName: 'Typography'
+    };
 
-	render() {
-		const headingsMarkup = (
+    render() {
+        const headingsMarkup = (
             <React.Fragment>
                 <h1>Heading 1</h1>
                 <h2>Heading 2</h2>
@@ -16,34 +16,44 @@ export default class Index extends React.Component {
                 <h4>Heading 5</h4>
                 <h6>Heading 6</h6>
             </React.Fragment>
-		);
+        );
 
-		const bodyMarkup = (
+        const bodyMarkup = (
             <React.Fragment>
                 <h1>Heading 1</h1>
-                <p>The Baltimore County Police Department was established by the Maryland State Legislature on April 11, 1874. The Maryland Legislature approved what became Chapter 374 of the Laws of Maryland. This authorized the Baltimore County Commissioners "to appoint such number of policemen as they may deem necessary, for the better protection of persons and property; the number not to exceed thirty at any one time, and to designate such number of said policemen as they may deem advisable, not exceeding five, as chief policemen."</p>
+                <p>
+                    The Baltimore County Police Department was established by
+                    the Maryland State Legislature on April 11, 1874. The
+                    Maryland Legislature approved what became Chapter 374 of the
+                    Laws of Maryland. This authorized the Baltimore County
+                    Commissioners "to appoint such number of policemen as they
+                    may deem necessary, for the better protection of persons and
+                    property; the number not to exceed thirty at any one time,
+                    and to designate such number of said policemen as they may
+                    deem advisable, not exceeding five, as chief policemen."
+                </p>
             </React.Fragment>
         );
 
-		const fonts = [
-			{
-				name: "Montserrat",
-				description: "Used for headings and emphasized text",
-				availableWeights: ["400","500","700"],
-				exampleMarkup: headingsMarkup
-			},
-			{
-				name: "Crimson Text",
-				description: "Used for body text and image captions",
-				availableWeights: ["400","400i"],
-				exampleMarkup: bodyMarkup
-			}
-		];
+        const fonts = [
+            {
+                name: 'Montserrat',
+                description: 'Used for headings and emphasized text',
+                availableWeights: ['400', '500', '700'],
+                exampleMarkup: headingsMarkup
+            },
+            {
+                name: 'Crimson Text',
+                description: 'Used for body text and image captions',
+                availableWeights: ['400', '400i'],
+                exampleMarkup: bodyMarkup
+            }
+        ];
 
-		return (
-			<React.Fragment>
-				<Headings fonts={fonts} />
-			</React.Fragment>
-		);
-	}
+        return (
+            <React.Fragment>
+                <Headings fonts={fonts} />
+            </React.Fragment>
+        );
+    }
 }
