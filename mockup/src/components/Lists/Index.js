37f79df302b679d378d60908c67dd501303e0b24
martypowell <jpowel25@gmail.com>
2018-11-14 11:20:50 -0500

    fixed unused reference

--- mockup/src/components/Lists/Index.js
+++ mockup/src/components/Lists/Index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Table } from 'antd';
+import { Card } from 'antd';
 import DocSection from '../DesignSystem/DocSection';
 import exampleLinkItems from '../../data/secondary-navigation.json';
 import linkListDocsData from '../../data/design-system/docs/link-list.json';
@@ -7,22 +7,26 @@ import BasicComponentInformation from '../DesignSystem/BasicComponentInformation
 import LinkList from './LinkList';
 
 export default class Index extends React.Component {
-	render() {
-		return (
-			<Card>
-				<BasicComponentInformation data={linkListDocsData} />
-				<DocSection>
-					<h3>Example</h3>
-					<DocSection>
-						<h4>Default</h4>
-						<LinkList items={exampleLinkItems} />
-					</DocSection>
-					<DocSection>
-						<h4>Grid</h4>
-						<LinkList items={exampleLinkItems.concat(exampleLinkItems)} grid columns={3} />
-					</DocSection>
-				</DocSection>
-			</Card>
-		);
-	}
+    render() {
+        return (
+            <Card>
+                <BasicComponentInformation data={linkListDocsData} />
+                <DocSection>
+                    <h3>Example</h3>
+                    <DocSection>
+                        <h4>Default</h4>
+                        <LinkList items={exampleLinkItems} />
+                    </DocSection>
+                    <DocSection>
+                        <h4>Grid</h4>
+                        <LinkList
+                            items={exampleLinkItems.concat(exampleLinkItems)}
+                            grid
+                            columns={3}
+                        />
+                    </DocSection>
+                </DocSection>
+            </Card>
+        );
+    }
 }
